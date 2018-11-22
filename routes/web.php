68fc3fabdf17d2717b536b57c0b41405b91f8370
sauliakarina <sauliakarina@gmail.com>
2018-11-22 14:10:06 +0700

    merge

--- routes/web.php
+++ routes/web.php
@@ -22,21 +22,13 @@ $router->get('/user', 'UserController@userHasLogin');
 $router->post('/kategori', 'KategoriController@store');
 $router->get('/kategori', 'KategoriController@index');
 $router->delete('/kategori/{id}', 'KategoriController@delete');
-<<<<<<< HEAD
 $router->post('/kategori/update/{id}', 'KategoriController@update');
-=======
-$router->put('/kategori/{id}', 'KategoriController@update'); //update blm bisa
->>>>>>> 1020a52421f0ca2fbee643f7c1ae34e4e6c0de92
 $router->get('/kategori/{id}', 'KategoriController@show');
 
 $router->post('/produk', 'ProdukController@store');
 $router->get('/produk', 'ProdukController@index');
 $router->delete('/produk/{id}', 'ProdukController@delete');
-<<<<<<< HEAD
 $router->post('/produk/update/{id}', 'ProdukController@update');
-=======
-$router->put('/produk/{id}', 'ProdukController@update'); //update blm bisa
->>>>>>> 1020a52421f0ca2fbee643f7c1ae34e4e6c0de92
 $router->get('/produk/{id}', 'ProdukController@show');
 
 
